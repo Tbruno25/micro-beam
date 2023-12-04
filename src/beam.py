a342e87ee9c02bd236709fe5419edee0a496fd62
TJ <tbruno25@gmail.com>
2023-12-03 16:38:42 -0800

    Add flash func

--- src/beam.py
+++ src/beam.py
@@ -12,21 +12,29 @@ frequency = 5_000
 pwm = PWM(pin, freq=frequency, duty=0)
 
 
-def fade_in():
+def fade_in() -> None:
     for i in RANGE:
         pwm.duty(i)
         sleep(DELAY)
 
 
-def fade_out():
+def fade_out() -> None:
     for i in RANGE[::-1]:
         pwm.duty(i)
         sleep(DELAY)
 
 
-def pulse(minutes: int):
+def pulse(minutes: int) -> None:
     seconds = minutes * 60
     start = time()
     while time() - start < seconds:
         fade_in()
         fade_out()
+
+
+def flash(times: int) -> None:
+    for _ in range(times):
+        pwm.duty(RANGE[-1])
+        sleep(0.2)
+        pwm.duty(0)
+        sleep(0.2)
